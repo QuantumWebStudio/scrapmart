98caf2358b400f47296056633a8fbeccfe0936b7
Ziyaan Ahmed <ziyaanahmed@icloud.com>
2024-10-17 16:34:33 +0530

    Added backend route for add to cart

--- app/api/product/[productid]/route.js
+++ app/api/product/[productid]/route.js
@@ -1,12 +1,18 @@
 import { connectToDatabase } from "@utils/connectDataBase";
 import { NextResponse } from "next/server";
 import Product from "@model/productModel";
+import Cart from "@model/cartModel";
+
+//This is used to conect the cloudnary platform
 import { connectCloudinary } from "@utils/connectCloudnary";
+
+//This is used to upload the files into the cloudinary and the uploaded url is stored in the cart database
 import {
   videoUpload,
   imageUpload,
   removeFile,
 } from "@helpers/cloudnaryOperations";
+import { cartDetail } from "@Constants/data";
 // Ensure the database is connected
 connectToDatabase();
 
@@ -42,33 +48,56 @@ export const GET = async (request, { params }) => {
 
 export const POST = async (request) => {
   try {
+    //this call is done to connect with the cloudinary platform
     connectCloudinary();
+
+    //Extracting or dsctructring the variables inside the formData
     const data = await request.formData();
     const productName = data.get("productName");
+    const productCategory = data.get("productCategory");
+    const productDescription = data.get("productDescription");
+    const productQuantity = data.get("productQuantity");
+    const productUnit = data.get("productUnit");
+    const productId = data.get("productId");
+    const productPrice = data.get("productPrice");
     const productImage = data.get("productImage");
-    // const productVideo = data.get("productVideo");
+    const productVideo = data.get("productVideo");
+
+    //This function returns the uploaded status of the image and video
     const imageData = await imageUpload(productImage);
-    // const videoData = await videoUpload(productVideo);
-    console.log(imageData);
+    const videoData = await videoUpload(productVideo);
+
     try {
+      const addedItems = await await Cart.create({
+        productId,
+        productName,
+        productUnit,
+        productPrice,
+        productQuantity,
+        productDescription,
+        productCategory,
+        productImage: imageData.secure_url,
+        prodcutVideo: videoData.secure_url,
+      });
       return NextResponse.json(
         {
-          singleProductDetail: productName,
+          msg: "Item Added to Cart",
+          cartDetail: addedItems,
         },
         { status: 200 }
       );
     } catch (error) {
-      console.error("Error fetching product:", error);
+      console.error("Failed to  add item to cart", error);
 
       // Return a 500 error response if there's an internal server error
       return NextResponse.json(
         {
-          error: "Internal Server Error",
+          error: "Something went wrong!!!",
         },
         { status: 500 }
       );
     }
   } catch (error) {
-    console.error("Error fetching product:", error);
+    console.error("Error in the server", error);
   }
 };
--- helpers/cloudnaryOperations.js
+++ helpers/cloudnaryOperations.js
@@ -3,31 +3,49 @@ import fs from "fs";
 
 //Used to upload the video file to the cloudinary
 const videoUpload = async (file) => {
-  try {
-    const data = await cloudinary.uploader.upload(file[0].path, {
-      folder: "VIDEO",
-      resource_type: "video",
-    });
-    return { url: data.secure_url, publicId: data.public_id };
-  } catch (err) {
-    console.log(err);
-    throw err;
-  }
+  const buffer = await file.arrayBuffer();
+  const bytes = Buffer.from(buffer);
+  return new Promise(async (resolve, reject) => {
+    await cloudinary.uploader
+      .upload_stream(
+        {
+          resource_type: "video",
+          folder: "VIDEO",
+        },
+        async (err, result) => {
+          if (err) {
+            return reject(err.message);
+          }
+          return resolve(result);
+        }
+      )
+      .end(bytes);
+  });
 };
 
 //Used to upload the image file to the cloudinary
 const imageUpload = async (file) => {
-  try {
-    const data = await cloudinary.uploader.upload(file[0].path, {
-      folder: "IMAGE",
-      resource_type: "image",
-    });
-    return { url: data.secure_url, publicId: data.public_id };
-  } catch (err) {
-    console.log(err);
-    throw err;
-  }
+  const buffer = await file.arrayBuffer();
+  const bytes = Buffer.from(buffer);
+  return new Promise(async (resolve, reject) => {
+    await cloudinary.uploader
+      .upload_stream(
+        {
+          resource_type: "image",
+          folder: "IMAGE",
+        },
+        async (err, result) => {
+          if (err) {
+            return reject(err.message);
+          }
+          return resolve(result);
+        }
+      )
+      .end(bytes);
+  });
 };
+
+
 
 //This is used to remove the files  from the local storage that is /pblic/tem after sucessfully being uploaded into the cloudinary
 
